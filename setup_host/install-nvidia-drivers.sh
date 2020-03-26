700dc350a62a4f94627519f68421ea02e73f41b2
Harish Anand <harishanand95@gmail.com>
2020-03-25 22:00:42 -0700

    Update install-nvidia-drivers.sh file
    Add CUDA support to K80 GPU

--- setup_host/install-nvidia-drivers.sh
+++ setup_host/install-nvidia-drivers.sh
@@ -1,3 +1,3 @@
-wget http://us.download.nvidia.com/XFree86/Linux-x86_64/390.25/NVIDIA-Linux-x86_64-390.25.run
-chmod a+x NVIDIA-Linux-x86_64-390.25.run
-./NVIDIA-Linux-x86_64-390.25.run -s --install-libglvnd
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/384.111/NVIDIA-Linux-x86_64-384.111.run
+chmod a+x NVIDIA-Linux-x86_64-384.111.run
+./NVIDIA-Linux-x86_64-384.111.run -s --install-libglvnd
