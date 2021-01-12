8c7d2350a4047e9c2d52d37425dead3dc0addfdc
Harish Anand <harishanand95@gmail.com>
2021-01-12 12:50:43 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,11 +15,20 @@ cd ..
 
 echo "********************************************************************************"
 echo ""
-echo "Creating the docker container."
+echo "Creating the docker images:"
+echo ""
+echo "1. openuav:base-cuda-10.2-ubuntu18.04"
+echo ""
+echo "2. openuav:ros-cuda-10.2-ubuntu18.04"
+echo ""
+echo "3. openuav:px4-cuda-10.2-ubuntu18.04"
 echo ""
 echo "********************************************************************************"
 
-docker build -t autonomous_sys_build autonomous_sys_build
+cd autonomous_sys_build/
+docker build -f Dockerfile.base -t openuav:base-cuda-10.2-ubuntu18.04 .
+docker build -f Dockerfile.ros -t openuav:ros-cuda-10.2-ubuntu18.04 .
+docker build -f Dockerfile.px4 -t openuav:px4-cuda-10.2-ubuntu18.04 .
 
 echo "********************************************************************************"
 echo ""
