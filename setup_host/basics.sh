b430309700fb97161004257f42b4ae5974be2922
Harish Anand <harishanand95@gmail.com>
2021-03-08 22:21:43 -0700

    Add architecture support for 32 bit systems
    NVIDIA CUDA installation fails because of missing 32-bit architecture support.

--- setup_host/basics.sh
+++ setup_host/basics.sh
@@ -2,3 +2,6 @@
 
 apt-get update -y
 apt-get install -y emacs lsof wget curl git htop less build-essential terminator make cmake net-tools lubuntu-desktop xvfb
+dpkg --add-architecture i386
+apt update -y
+apt install libc6:i386 -y
